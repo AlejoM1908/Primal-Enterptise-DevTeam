5a5d7f17cb796b8f9646837a6fcf0f438f1ee59a
mbolanoss <53063734+mbolanoss@users.noreply.github.com>
2020-12-03 20:11:15 -0500

    Added update on product info

--- src/lib/ui/consultProductView/ConsultProductModel.java
+++ src/lib/ui/consultProductView/ConsultProductModel.java
@@ -7,12 +7,15 @@ package lib.ui.consultProductView;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.InputMismatchException;
+import javax.swing.JOptionPane;
 import lib.app.DBConnection;
 import lib.ui.MainApp.MainAppController;
 
 
 public class ConsultProductModel {
     
+    private int productId;
     
     private MainAppController rootComponent;
 
@@ -26,7 +29,7 @@ public class ConsultProductModel {
         this.view = view;
     }
     
-    public void fillInfoProduct (int productId) throws SQLException{
+    public void fillInfoProduct () throws SQLException{
     
       DBConnection conection = new DBConnection();
       conection.getConnection();
@@ -39,13 +42,40 @@ public class ConsultProductModel {
       String almacenamientoInfo = resultInfo.getString(6);
       String descripcionInfo = resultInfo.getString(2);
       String fechaCaducidadInfo = resultInfo.getString(7);
-      view.getNombreInfolb().setText(nombreInfo);
-      view.getTipoInfolb().setText(tipoInfo);
-      view.getCantidadInfolb().setText(cantidadInfo);
-      view.getAlmacenamientoInfolb().setText(almacenamientoInfo);
+      String metodoAlmacenamiento = resultInfo.getString(5);
+      view.getJtxtName().setText(nombreInfo);
+      view.getJtxtType().setText(tipoInfo);
+      view.getJtxtAmount().setText(cantidadInfo);
+      view.getJtxtPlace().setText(almacenamientoInfo);
+      view.getJtxtMethod().setText(metodoAlmacenamiento);
       view.getJtxtaDescripcion().setText(descripcionInfo);
-      view.getFechaCaducidadInfolb().setText(fechaCaducidadInfo);
+      view.getJcbxYear().setSelectedItem(fechaCaducidadInfo.substring(0, 4));
+      view.getJcbxMonth().setSelectedItem(fechaCaducidadInfo.substring(5, 7));
+      view.getJcbxDay().setSelectedItem(fechaCaducidadInfo.substring(8, 10));
     
+    }
+    
+    public void updateInfo(){
+        try {
+            int amount = Integer.parseInt(view.getJtxtAmount().getText());
+            String name = "\"" + view.getJtxtName().getText() + "\"";
+            String type = "\"" + view.getJtxtType().getText() + "\"";
+            String place = "\"" + view.getJtxtPlace().getText() + "\"";
+            String method = "\"" + view.getJtxtMethod().getText() + "\"";
+            String desc = "\"" + view.getJtxtaDescripcion().getText() + "\"";
+            String year = (String) view.getJcbxYear().getSelectedItem();
+            String month = (String) view.getJcbxMonth().getSelectedItem();
+            String day = (String) view.getJcbxDay().getSelectedItem();
+            String date = "\"" + year + "-" + month + "-" + day + "\"";
+            DBConnection conn = new DBConnection();
+            conn.getConnection();
+            conn.executeQuery("CALL updateProductInfo(" + Integer.toString(productId) + "," 
+                              + Integer.toString(amount) + "," + name + ","+ type + ","
+                              + place + ","+ method + ","+ desc + ","+ date + ");");
+            conn.endCOnnection();
+        } catch (InputMismatchException e) {
+            JOptionPane.showMessageDialog(view, "La cantidad debe ser un número", "", JOptionPane.WARNING_MESSAGE);
+        }
     }
 
     public void setView(ConsultProductView view) {
@@ -58,6 +88,14 @@ public class ConsultProductModel {
 
     public ConsultProductView getView() {
         return view;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
     }
     
 }
